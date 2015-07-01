58ca761e3ab9fc51265062120f47027b3c9adf0a
Lance Hudson <lance@clariturehealth.com>
2015-06-30 21:32:17 -0400

    remove try 1

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 "use strict";
 
 (function() {
-  var config = require('./config.json');
-  var udpServer = require('./lib/udpServer.js')(config);
-  var db = require('./lib/db.js')(config);
-  var serial = require('./lib/serial.js')(config);
-  var app = require('./lib/app.js')(config, db, serial);
-
-  udpServer.start();
-  db.start();
-  udpServer.on('message', app.messageHandler);
+  // stuff here
 })();
