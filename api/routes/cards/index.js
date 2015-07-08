8c03ce883d410f7740ed87d222ac1b0ba01a5e78
Lance Hudson <lance@clariturehealth.com>
2015-07-08 02:08:20 -0400

    you have to select something

--- api/routes/cards/index.js
+++ api/routes/cards/index.js
@@ -54,7 +54,7 @@ Cards.deleteById = function(req, res) {
 };
 
 Cards.isUIDAllowed = function(uid, cb) {
-  db.get('SELECT FROM cards JOIN members on members.ROWID = cards.memberId WHERE cards.enabled = "1" AND members.enabled = "1" AND cards.uid = "$uid"', [uid], function(err, row) {
+  db.get('SELECT cards.UID FROM cards JOIN members on members.ROWID = cards.memberId WHERE cards.enabled = "1" AND members.enabled = "1" AND cards.uid = "$uid"', [uid], function(err, row) {
     if(err) return cb(err);
     if(row) return cb(null, true);
     else return cb(null, false);
