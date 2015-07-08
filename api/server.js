03b85bc30b1097658b29689a63f28a23b2df1e61
Lance Hudson <lance@clariturehealth.com>
2015-07-08 02:41:03 -0400

    strip carraige return and linefeed from string

--- api/server.js
+++ api/server.js
@@ -32,6 +32,7 @@ app.listen(config.app.port, config.app.host, function(e) {
           console.log('failed to open: '+ error);
         } else {
           serialPort.on('data', function(data) {
+            data = data.toString().replace('/r', '').replace('/n', '');
             cards.isUIDAllowed(data, function(err, allowed) {
               if (err) throw err;
               if (allowed) door.open();
