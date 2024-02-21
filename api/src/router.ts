d4c9981f9a0b1a79919ef8cfb1d5f5eb3936e62b
Matthew Hoover <matthew@hooverdev.com>
2024-02-21 13:48:13 -0500

    Added Swagger API Explorer for Auth Server

--- api/src/router.ts
+++ api/src/router.ts
@@ -1,17 +1,33 @@
-import { useContainer, useExpressServer } from "routing-controllers";
-import { Container } from "typedi";
 import { Router } from "express";
+import { getMetadataArgsStorage, RoutingControllersOptions, useContainer, useExpressServer } from "routing-controllers";
+import { routingControllersToSpec } from "routing-controllers-openapi";
+import { serve, setup } from "swagger-ui-express";
+import { Container } from "typedi";
 import "reflect-metadata";
 
 import { AccessController } from "./controllers/access-controller.js";
 import "./logger.js";
 
-export const router = Router()
+// Declare router
+export const router = Router();
 
-useContainer(Container);
-useExpressServer(router, {
+// Define configuration for RoutingControllers
+const options: RoutingControllersOptions = {
     routePrefix: "/api",
     controllers: [
         AccessController,
     ]
+};
+
+// Setup routing-controllers server
+useContainer(Container);
+useExpressServer(router, options);
+
+// Setup Swagger explorer
+const swaggerSpec = routingControllersToSpec(getMetadataArgsStorage(), options, {
+    info: {
+        title: "FamiLAB Access Control",
+        version: "4"
+    }
 });
+router.use('/explorer', serve, setup(swaggerSpec));
