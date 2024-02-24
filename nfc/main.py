f6787149b99ae79438ead412588224b196bd922b
Matthew Hoover <matthew@hooverdev.com>
2024-02-24 15:18:43 -0500

    Added relay pin

--- nfc/main.py
+++ nfc/main.py
@@ -3,11 +3,11 @@ from os import uname
 
 import board
 from busio import SPI
+from digitalio import DigitalInOut, Direction
 
 from src.display import Display
 from src.reader import Reader
 from src.wifi import Wifi
-
 
 # Setup
 print("\n\n")
@@ -22,6 +22,7 @@ display: Display | None = None
 wifi: Wifi | None = None
 
 board_id = uname()[0]
+relay: DigitalInOut
 if board_id == 'ESP32S3':
     spi = board.SPI()
     display = Display(spi)
@@ -32,12 +33,17 @@ if board_id == 'ESP32S3':
     display.draw_text("Connecting\nto WiFi")
     wifi = Wifi()
 
+    relay = DigitalInOut(board.D13)
+    relay.direction = Direction.OUTPUT
+
 elif board_id == 'rp2040':
     spi = SPI(board.GP18, board.GP19, board.GP16)
     reader = Reader(spi, board.GP17, board.GP20)
+
+    # relay = DigitalInOut(board.D13)
+    # relay.direction = Direction.OUTPUT
 else:
     raise RuntimeError(f"Unsupported platform {board_id}")
-
 
 # Loop
 while True:
@@ -60,8 +66,10 @@ while True:
     if can_access:
         if display:
             display.draw_text("Access Granted", 0x00FF00)
+            relay.value = 1
     else:
         if display:
             display.draw_text("Access Denied", 0xFF0000)
 
     time.sleep(3)
+    relay.value = 0
